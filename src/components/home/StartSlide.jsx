9848b59492b3e57f8944fc31d1dd61a34bffefdd
Francisco Solis <fran@theprogramsrc.xyz>
2022-09-17 17:58:24 -0300

    Fixes

--- src/components/home/StartSlide.jsx
+++ src/components/home/StartSlide.jsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export default function StartSlide({ normalTitle, boldTitle, text, link1, link2, image }) {
     const navigate = (e, url) => {
         e.preventDefault();
@@ -42,7 +40,7 @@ export default function StartSlide({ normalTitle, boldTitle, text, link1, link2,
                 </div>
             </div>
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                <Image layout="fill" className="lg:rounded-r-xl h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={image} alt=""/>
+                <img className="lg:rounded-r-xl h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={image} alt=""/>
             </div>
         </div>
     );
